ca0b3e68c66f3b9f531e5c19c2773615f1564d51
Jonb <jonpbiv@gmail.com>
2016-06-03 19:12:04 -0400

    project made

---
+++ Tester/Tester/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tester.Controllers
+{
+    public class HomeController : Controller
+    {
+        /** http://localhost:62113/ **/
+
+        public string Index()
+        {
+            return "Hello World";
+        }
+
+        /**public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }**/
+    }
+}
---
+++ Tester/Tester/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tester.Startup))]
+namespace Tester
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
